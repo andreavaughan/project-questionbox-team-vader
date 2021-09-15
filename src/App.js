23ce344c4cf99f50e9d4f38bf68630fb408e441a
andreavaughan <amvaughan16@gmail.com>
2021-09-15 18:24:10 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 import { questions as data } from './test-data'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Nav } from './components/Nav'
 import { Questions } from './components/Questions'
+import { Profile } from './components/Profile'
+
 
 export const App = () => {
     const [questions] = useState(data)
 
     return (
-        <div className="">
-            <Nav />
-            <Questions questions={questions} />
-        </div>
+        <Router>
+            <div className="app">
+                <Nav />
+                <Switch>
+                    <Route exact path="/" component={() => <Questions questions={questions} />} />
+                    <Route path="/profile" component={Profile} />
+                </Switch>
+            </div>
+        </Router>
     )
 }
