4d67359b0ea4adcabf329ccab028946bddc66b46
andreavaughan <amvaughan16@gmail.com>
2021-09-15 10:09:42 -0400

    added test data

---
+++ src/test-data.js
@@ -0,0 +1,39 @@
+export const questions = [
+    {
+        question: "Why is the sky blue?",
+        author: "username1",
+        created_date: "2012-03-09T06:54:45.213Z",
+        answers: {
+            answer1: {
+                answer: "because it is",
+                author: "username2",
+                created_date: "2012-03-10T06:54:45.213Z",
+                answer_accepted: false,
+                bookmarked: false
+            },
+            answer2: {
+                answer: "It has to do with how water vapor reflects the light",
+                author: "username3",
+                created_date: "2012-03-11T06:54:45.213Z",
+                answer_accepted: true,
+                bookmarked: true
+            }
+        },
+        bookmarked: false
+    }, 
+    {
+        question: "How long is the average year?",
+        author: "username2",
+        created_date: "2012-03-09T06:54:45.213Z",
+        answers: {
+            answer1: {
+                answer: "365 days",
+                author: "username1",
+                created_date: "2012-03-10T06:54:45.213Z",
+                answer_accepted: true,
+                bookmarked: false
+            }
+        },
+        bookmarked: false
+    }
+]
