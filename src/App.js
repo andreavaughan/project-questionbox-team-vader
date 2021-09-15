457eaa08dbf7e2b1a1c7d650f8525bdbc8843a5e
andreavaughan <amvaughan16@gmail.com>
2021-09-15 13:14:00 -0400

    basic question component rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { questions as data } from './test-data'
 import { Questions } from './components/Questions'
 
 export const App = () => {
-    
+    const [questions] = useState(data)
+
     return (
         <div className="">
             <h1>Project Questionbox</h1>
+            <Questions questions={questions} />
         </div>
     )
 }
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import '../styles/questions.css'
 
-export const Questions = () => {
+export const Questions = ({ questions }) => {
+    console.log(questions)
+    
     return (
-        <div>
-            
-        </div>
+        <>
+            { questions.map((question, idx) => (
+                <div className="card" key={idx}>
+                    <div className="card-body">
+                        <blockquote className="blockquote">
+                            <p>{question.question}</p>
+                        </blockquote>
+                    </div>
+                    <div className="card-footer text-muted">
+                        <p>{question.created_date}</p>
+                        <p>{question.author}</p>
+                        <button className="btn btn-light">
+                            View answers
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </>
     );
 }
 
