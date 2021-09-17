43fa5baefadc320339d1ef994776a111ab61d4bc
andreavaughan <amvaughan16@gmail.com>
2021-09-17 14:52:14 -0400

    added api to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const Profile = ({ token }) => {
+    const [ user, setUser ] = useState('')
+    
     useEffect(() => {
-        // axios.get('', {
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Authorization': `token ${token}`
-        //     }
-        // }).then((response) => response.data)
+        axios.get('https://questionbox1.herokuapp.com/auth/users/me/', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `token ${token}`
+            }
+        }).then((response) => setUser(response.data))
     })
     
     return (
         <div>
             <h1>Profile</h1>
+            <h3>{user.username}</h3>
+            <p>{user.first_name} {user.last_name}</p>
         </div>
     );
 }
