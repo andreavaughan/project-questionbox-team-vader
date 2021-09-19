3086400629eefd39c8366d86089c1d599b8e7652
andreavaughan <amvaughan16@gmail.com>
2021-09-19 14:12:33 -0400

    added details to add question form

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import axios from 'axios'
+import '../styles/add-question.css'
 
 export const AddQuestion = () => {
-    const [ newQuestion, setNewQuestion ] = useState('')
+    const [ newQuestionTitle, setNewQuestionTitle ] = useState('')
+    const [ details, setDetails ] = useState('')
     // const [ qAuthor, setQAuthor ] = useState('')
     const history = useHistory()
     
     const handleChange = (inputType, event) => {
-        if (inputType === 'newQuestion'){
-            setNewQuestion(event.target.value)
+        if (inputType === 'newQuestionTitle'){
+            setNewQuestionTitle(event.target.value)
+        }
+        if (inputType === 'details'){
+            setDetails(event.target.value)
         }
     }
 
@@ -17,7 +22,8 @@ export const AddQuestion = () => {
         event.preventDefault()
         axios.post('https://questionbox1.herokuapp.com/api/questions/',
             {
-                question: newQuestion,
+                "question": newQuestionTitle,
+                "details": details,
                 // author: qAuthor
             })
             .then(response => {
@@ -34,11 +40,18 @@ export const AddQuestion = () => {
         <>
             <form className='form question-form' onSubmit={handleSubmit}>
                 <label className="label">Ask your question:</label>
+                <input 
+                    className="input form-control"
+                    type="text"
+                    value={newQuestionTitle}
+                    onChange={(event) => handleChange('newQuestionTitle', event)}
+                />
+                <label className="label">Details:</label>
                 <textarea 
                     className="input form-control"
                     type="text"
-                    value={newQuestion}
-                    onChange={(event) => handleChange('newQuestion', event)}
+                    value={details}
+                    onChange={(event) => handleChange('details', event)}
                 />
                 <button type="submit" className="btn btn-secondary">Submit question</button>
                 <button className="btn btn-light">Cancel</button>
