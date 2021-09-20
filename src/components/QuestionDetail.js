35957a2d2dede06752bc65bde0d32e283fe5a5c5
andreavaughan <amvaughan16@gmail.com>
2021-09-19 23:10:47 -0400

    in-progress answers on question details page rendering

--- src/components/QuestionDetail.js
+++ src/components/QuestionDetail.js
@@ -24,6 +24,8 @@ export const QuestionDetail = ({ token, questionID, isLoading, setIsLoading }) =
 
     }, [setIsLoading])
     
+    console.log(typeof(questionDetail.answers))
+
     return isLoading ?
         <>
             <strong>Loading...</strong>
@@ -49,7 +51,21 @@ export const QuestionDetail = ({ token, questionID, isLoading, setIsLoading }) =
                     </div>
                     <div>
                         <h2>Answers</h2>
-                        
+                        <div className="a-body">
+                            {/* { questionDetail.answers.map((answer, idx) => {
+                                <div className="card" key={idx}>
+                                    <div className="card-body">
+                                        <blockquote className="blockquote">
+                                            <p>{answer.answer}</p>
+                                        </blockquote>
+                                    </div>
+                                    <div className="card-footer text-muted q-detail">
+                                        <p>Added: {answer.created_date}</p>
+                                        <p>Submitted by: {answer.author}</p>
+                                    </div>
+                                </div>
+                            })} */}
+                        </div>
                     </div>
                 </>
             )}
